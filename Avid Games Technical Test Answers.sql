d635d1946a30d77029dbcc7ba79bcdfe34ef84fe
Mihey <85169396+Mihey-Matev@users.noreply.github.com>
2023-04-04 01:25:36 +0100

    refactor

--- Avid Games Technical Test Answers.sql
+++ Avid Games Technical Test Answers.sql
@@ -9,16 +9,23 @@ BEGIN
   ELSE
     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid JSON input', MYSQL_ERRNO = 333;
   END IF;
-  
+
   WITH recursive `cte` (`ind`, `key`, `value`) AS (
       SELECT -1, CAST('' AS VARCHAR(255)), CAST('' AS VARCHAR(255))  
       UNION
-      
-      SELECT (`ind` + 1), 
-             JSON_UNQUOTE(JSON_EXTRACT(JSON_KEYS(`json`), CONCAT('$[', (`ind` + 1),']'))), 
-             JSON_UNQUOTE(JSON_EXTRACT(`json`, CONCAT('$.', JSON_EXTRACT(JSON_KEYS(`json`), CONCAT('$[', (`ind` + 1),']')))))
-      FROM `cte`
-      WHERE `ind` < JSON_LENGTH(`json`) - 1
+
+      SELECT 
+      	`i` AS `ind`, 
+      	JSON_UNQUOTE(`key_i`) AS `key`, 
+      	JSON_UNQUOTE(JSON_EXTRACT(`json`, CONCAT('$.', `key_i`))) AS `value`
+      FROM
+      (
+          SELECT 
+          	JSON_EXTRACT(JSON_KEYS(`json`), CONCAT('$[', (`ind` + 1),']')) AS `key_i`,
+          	`ind` + 1 AS `i`
+          FROM `cte`
+          WHERE `ind` < JSON_LENGTH(`json`) - 1
+      )      AS outerTable
   )  
   SELECT `key`, `value` FROM `cte` WHERE `ind` != -1;
   
